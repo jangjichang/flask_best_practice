e7e47438774f9fdb48f827dec0d22a6a4f3357f5
Jichang Jang <devjang@crossangle.io>
2020-07-24 00:02:51 +0900

    Remove to command directory

---
+++ command/remove_cache_file.sh
@@ -0,0 +1,2 @@
+# find . -depth 1 -type d -name '__pycache__' -exec rm -rf {} \;
+# find . -depth 1 -type d -name '.pytest_cache' -exec rm -rf {} \;
--- remove_cache_file.sh
+++
@@ -1,2 +0,0 @@
-find . -depth 1 -type d -name '__pycache__' -exec rm -rf {} \;
-find . -depth 1 -type d -name '.pytest_cache' -exec rm -rf {} \;
