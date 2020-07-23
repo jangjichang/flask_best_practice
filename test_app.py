4b3b841517a620ece1f5f87db2ff49a53770c22d
Jichang Jang <devjang@crossangle.io>
2020-07-24 00:03:55 +0900

    Add movie api test

--- test_app.py
+++ test_app.py
@@ -1,8 +1,39 @@
+import pytest
+import random
 from .app import app
 
-def test_hello_world():
-    client = app.test_client()
-    response = client.get('/')
+
+@pytest.fixture
+def test_client():
+    return app.test_client()
+
+@pytest.fixture
+def random_int():
+    return random.randint(1, 10)
+
+def test_get_movies(test_client):
+    response = test_client.get('/movies')
     assert response.status_code == 200
-    assert b'data' in response.data
-    assert b'Hello, World' in response.data
+    assert '{"data": "movies"}' == response.data.decode()
+
+def test_post_movies(test_client):
+    response = test_client.post('/movies')
+    assert response.status_code == 201
+    assert '{"message": "created"}' == response.data.decode()
+
+def test_put_movie(test_client, random_int):
+    response = test_client.put(f'/movies/{random_int}')
+    assert response.status_code == 200
+    assert f'{{"message": "put {random_int}"}}' == response.data.decode()
+
+def test_delete_movie(test_client, random_int):
+    response = test_client.delete(f'/movies/{random_int}')
+    assert response.status_code == 200
+    assert f'{{"message": "deleted {random_int}"}}' == response.data.decode()
+
+def test_get_movie(test_client, random_int):
+    response = test_client.get(f'/movies/{random_int}')
+    assert response.status_code == 200
+    assert f'{{"data": "movie number {random_int}"}}' == response.data.decode()
+
+    
